704a64cf3d0ccd95f9d6a2575b64f405b8654f30
Admin <hanhngao@gmail.com>
2022-09-27 15:19:37 +0700

    'add'

---
+++ dist/src/controller/admin.controller.d.ts
@@ -0,0 +1,5 @@
+import { Request, Response, NextFunction } from "express";
+export declare class AdminController {
+    constructor();
+    showAdminPage(req: Request, res: Response, next: NextFunction): void;
+}
